6e9fc95432a7504c0890be749eb18bddf6a552f0
Henk Reder <henk@anestuary.com>
2014-10-14 00:00:31 -0400

    added some model stuff

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141008225814) do
+ActiveRecord::Schema.define(version: 20141013230154) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20141008225814) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "zip_code"
   end
 
   create_table "line_items", force: true do |t|
--- sequel-assignment/sequel-assignment.sql
+++ sequel-assignment/sequel-assignment.sql
@@ -1,17 +1,18 @@
 
 # 1. Print out the number of orders per customer
-
-SELECT count(*) as order_count, customer_id
-FROM orders
-GROUP BY customer_id;
+select customer_id, count(*)
+from orders
+group by customer_id
+order by count(*) desc;
 
 # 2. Print out the top 5 customers (placed the most orders)
 
-SELECT count(*) as order_count, customer_id
-FROM orders
-GROUP BY customer_id
-ORDER BY order_count desc
-LIMIT 5;
+SELECT o.id as order_id, sum(li.quantity * p.price)
+from orders o
+join line_items li on li.order_id = o.id
+join products p on li.product_id = p.id
+group by o.id
+order by sum(li.quantity * p.price) desc;
 
 # 3. Print out the total amount per order
 
@@ -19,9 +20,18 @@ SELECT id, total_amount
 FROM orders
 ORDER BY id;
 
+# or
+
+SELECT o.id, sum(li.quantity * p.price)
+from orders o
+join line_items li on li.order_id = o.id
+join products p on li.product_id = p.id
+group by o.id
+order by o.id;
+
 # 4. Print out the emails that have placed more than 6 orders
 
-SELECT DISTINCT c.email as email,
+SELECT c.email as email,
         count(o.id) as order_count
         FROM orders o
         JOIN customers c on c.id = o.customer_id
@@ -32,8 +42,8 @@ SELECT DISTINCT c.email as email,
 
 # 5. Delete the last 5 orders
 DELETE from orders
-where id in (SELECT id FROM orders
-  ORDER BY id desc
+where created_at in (SELECT created_at FROM orders
+  ORDER BY created_at desc
   LIMIT 5);
 
 # 6. Copy all the orders to a CSV file
