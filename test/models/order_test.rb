41c09568ad31688707a06560a2d054aa914c59af
Henk Reder <henk@anestuary.com>
2014-10-15 19:15:10 -0400

    added tests for order

--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class OrderTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+   test "create an order" do
+     o = Order.new(total_amount: 8)
+     assert o.save, o.errors.full_messages
+   end
+
+   test "total amount is not a number" do
+     o = Order.new
+     refute o.save, 'total amount should be a number'
+     assert o.errors.full_messages.include?("Total amount is not a number")
+   end
 end
